4d7d209a55ff56480ac6066fbeab27ae1b11bc32
sunkay <sunkay11@gmail.com>
2018-04-05 07:31:04 -0400

    signup finito

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -41,7 +41,19 @@ export default class Signup extends Component{
         event.preventDefault();
 
         this.setState({isLoading: true});
-        this.setState({newUser: "test"});
+
+        try {
+            const newUser = await Auth.signUp({
+                username: this.state.email,
+                password: this.state.password
+            });
+            this.setState({newUser});
+
+        } catch(e){
+            console.log(e.message);
+            alert(e.message);
+        }
+
         this.setState({isLoading: false});
     }
 
@@ -49,6 +61,24 @@ export default class Signup extends Component{
         event.preventDefault();
 
         this.setState({isLoading: true});
+
+        try{
+            await Auth.confirmSignUp(
+                this.state.email, 
+                this.state.confirmationCode
+            );
+            await Auth.signIn(
+                this.state.email,
+                this.state.password
+            );
+
+            this.props.userHasAuthenticated(true);
+            this.props.history.push("/")
+        } catch(e){
+            console.log(e.message);
+            alert(e.message);            
+            this.setState({isLoading: false});
+        }
     }
 
     renderConfirmationForm(){
