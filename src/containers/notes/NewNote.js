ede099e0628a56503cff9a95092de04951735b12
sunkay <sunkay11@gmail.com>
2018-04-10 16:13:19 -0400

    upload attachment to S3

--- src/containers/notes/NewNote.js
+++ src/containers/notes/NewNote.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../../components/LoaderButton";
-import "./NewNote.css";
-import config from "../../config";
 import { API } from "aws-amplify";
 
+import "./NewNote.css";
+import config from "../../config";
+import { s3Upload } from "../../libs/awsLib";
 
 export default class NewNote extends Component{
     constructor(props){
@@ -39,7 +40,13 @@ export default class NewNote extends Component{
         this.setState({isLoading: true});
 
         try{
+
+            const attachment = this.file
+                ? await s3Upload(this.file)
+                : null;
+
             await this.createNote({
+                attachment,
                 content: this.state.content
             });
             this.props.history.push("/");
