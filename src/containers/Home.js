5d6640c061b55558de7977b74915bd3c138942e7
sunkay <sunkay11@gmail.com>
2018-04-11 07:33:24 -0400

    Call serverless list API

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,93 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { API } from "aws-amplify";
 import "./Home.css";
 
 class Home extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            notes: []
+        }
+    }
+
+    async componentDidMount(){
+        if (!this.props.isAuthenticated){
+            return;
+        }
+
+        try{
+            const notes = await this.notes();
+            this.setState({ notes });
+        } catch(e){
+            console.log(" Home:componentDidMount -> " + e);
+            alert(e);
+        }
+        this.setState({ isLoading: false});
+    }
+
+    notes(){
+        return API.get("notes", "/notes");
+    }
+
+    renderNotesList(notes){
+        return [{}].concat(notes).map(
+            (note, i) =>
+            i !== 0
+                ? <ListGroupItem
+                    key={note.noteId}
+                    href={`/notes/${note.noteId}`}
+                    onClick={this.handleNoteClick}
+                    header={note.content.trim().split("\n")[0]}
+                >
+                    {"Created: " + new Date(note.createdAt).toLocaleString()}
+                </ListGroupItem>
+                : <ListGroupItem
+                    key="new"
+                    href="/notes/new"
+                    onClick={this.handleNoteClick}
+                >
+                    <h4>
+                    <b>{"\uFF0B"}</b> Create a new note
+                    </h4>
+                </ListGroupItem>
+        );
+    }
+
+    renderLander(){
+        return(
+            <div className="lander">
+                <h1>Scratch</h1>
+                <p>A simple notes app</p>
+            </div>
+        );
+    }
+
+    renderNotes(){
+        return(
+            <div className="notes">
+                <PageHeader> Your Notes </PageHeader>
+                <ListGroup>
+                    {!this.state.isLoading 
+                      && this.renderNotesList(this.state.notes)
+                    }
+                </ListGroup>
+            </div>            
+        );
+    }
+
     render(){
+        //console.log(this.props)
         return (
             <div className="Home">
-                <div className="lander">
-                    <h1>Scratch</h1>
-                    <p>A simple notes app</p>
-                </div>
+                {
+                    this.props.isAuthenticated 
+                    ? this.renderNotes()
+                    : this.renderLander()
+                }
             </div>
         );
     }
