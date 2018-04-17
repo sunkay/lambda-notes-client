235db11054eb9f2f417345e81e4330095d781213
sunkay <sunkay11@gmail.com>
2018-04-17 15:23:52 -0400

    finish testing Home component including mocked functions returning a promise

--- src/containers/Home.test.js
+++ src/containers/Home.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link, MemoryRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from 'react-router-bootstrap';
 import { shallow, mount, render, simulate } from 'enzyme';
@@ -14,15 +14,16 @@ it('renders lander when unauthenticated', () =>{
 });
 
 it('renders notes when authenticated', () =>{
-    const wrapper = shallow(<Home />);
+    const wrapper = shallow(<Home  />);
     wrapper.setProps({isAuthenticated: true})
-    //console.log(wrapper.debug());
+
     expect(wrapper.find('.notes').length).toBe(1);
     expect(wrapper.find(ListGroup).length).toBe(1);
 });
 
 it('renders a correct list of notes when authenticated', () =>{
     const wrapper = shallow(<Home />);
+
     wrapper.setState({
         isLoading: false,
         notes: [
@@ -39,7 +40,7 @@ it('renders a correct list of notes when authenticated', () =>{
         ]
     });
     wrapper.setProps({isAuthenticated: true})
-    //console.log(wrapper.debug());
+
     expect(wrapper.find('.notes').length).toBe(1);
     expect(wrapper.find(ListGroupItem).length).toBe(3);
 });
@@ -62,39 +63,38 @@ it('renders an empty list of notes when isLoading is true', () =>{
         ]
     });
     wrapper.setProps({isAuthenticated: true})
-    //console.log(wrapper.debug());
     expect(wrapper.find('.notes').length).toBe(1);
     expect(wrapper.find(ListGroupItem).length).toBe(0);
 });
 
 
-it.skip('mount test', () =>{
-    const wrapper = mount(<MemoryRouter><Home /></MemoryRouter>);
-    wrapper.setProps({
-        children: React.cloneElement(wrapper.props().children, { isAuthenticated: true }),
-    });
-    //console.log(wrapper.find(Home).instance());
-    const spy = jest.spyOn(wrapper.find(Home).instance(), 'notes').mockImplementation(() => {
-        notes: [
+it('test componentDidMount and fetching mocked data', async () =>{
+
+  const spy = jest.spyOn(Home.prototype, 'notes').
+    mockImplementation(() => {
+      return Promise.resolve([
             {
                 noteId: 1,
-                content: "Test1 Content",
+                content: "Mocked Test1 Content",
                 createdAt: 1522596848355
             },
             {
                 noteId: 2,
-                content: "Test2 Content",
+                content: "Mocked Test2 Content",
                 createdAt: 1522596848355
-            }
-        ]
-        
+            },
+      ]);
     });
 
-    wrapper.find(Home).instance().forceUpdate();    
+  const wrapper = await shallow(<Home isAuthenticated={true} /> );
 
-    console.log(wrapper.debug());
+  wrapper.setState({isLoading: false});
+  expect(spy).toHaveBeenCalled();
+  expect(spy).toHaveBeenCalledTimes(1);
+  //console.log(wrapper.debug());
+  expect(wrapper.find(ListGroupItem).length).toBe(3);
+  expect(wrapper.find('[header="Mocked Test1 Content"]').length).toBe(1);
+  spy.mockReset();
+  spy.mockRestore();
 
-    expect(spy).toHaveBeenCalled();
-    expect(wrapper.find('.notes').length).toBe(1);
-    expect(wrapper.find(ListGroupItem).length).toBe(0);
 });
